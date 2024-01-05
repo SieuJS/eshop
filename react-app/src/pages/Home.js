5cb2eaa090f2400506cbb0863741d909c8973784
Quoc Hoang <hoangvanquoc923@gmail.com>
2024-01-05 17:46:15 +0700

    convert some components

---
+++ react-app/src/pages/Home.js
@@ -0,0 +1,12 @@
+import react from "react";
+import Topbar from "../components/Topbar.js"
+import Navbar from "../components/Navbar.js"
+
+export default function Home() {
+    return (
+        <>
+            <Topbar />
+            <Navbar />
+        </>
+    );
+}
