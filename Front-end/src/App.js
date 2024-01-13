379a8a2fe6ac353db1bb6f660e58019e9b436e33
21120561 <ducthinh12tn137@gmail.com>
2024-01-13 10:42:26 +0700

    update UI FE admin product + categories

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
-//import Home from "./pages/Home.js";
+import Home from "./pages/Home.js";
 import Detail from "./pages/Detail.js";
 import Cart from "./pages/Cart.js";
 import ProductList from "./pages/ProductList.js"
+import Product from './pages/Admin/Products';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
-        <ProductList />
+        <BrowserRouter>  
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/admin/product' exact element={<Product/>} />
+          </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ Front-end/src/components/Admin/AdminHeader.js
@@ -0,0 +1,17 @@
+export default function AdminHeader() {
+    return (
+        <header className="header">
+            <div className="menu-icon" onclick="openSidebar()">
+                <span className="material-icons-outlined">menu</span>
+            </div>
+            <div className="header-left">
+                <span className="material-icons-outlined">search</span>
+            </div>
+            <div className="header-right">
+                <span className="material-icons-outlined">notifications</span>
+                <span className="material-icons-outlined">email</span>
+                <span className="material-icons-outlined">account_circle</span>
+            </div>
+        </header> 
+    );
+};
