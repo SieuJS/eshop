de3c3f51b62b0c37138eb1c72776a2594a3068d2
21120538 <luuvinhquangqg@gmail.com>
2024-01-19 15:23:21 +0700

    xóa categories.c và categories.m (dùng category của quốc)

--- Back-end/models/categories.m.js
+++
@@ -1,8 +0,0 @@
-const db = require('../utils/db')
-
-module.exports = class Categories {
-    static async getAll() {
-        const rs = await db.query(`SELECT * FROM "Categories"`)
-        return rs;
-    }
-}
