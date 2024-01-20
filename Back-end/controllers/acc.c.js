b3c11deff359cfdd860250f8151bf5002e299f41
21120321 <gamefun206@gmail.com>
2024-01-20 09:50:09 +0700

    add hooks, http jwt toeken

--- Back-end/controllers/acc.c.js
+++ Back-end/controllers/acc.c.js
@@ -1,156 +1,197 @@
 const accM = require("../models/acc.m");
 const bcrypt = require("bcrypt");
-const HttpError = require("../models/http-error");
+const jwt = require("jsonwebtoken");
 const saltRound = 10;
+const HttpError = require("../models/http-error");
+// Quy uoc loi input tu client la 420
 
 module.exports = {
-    getUserById: async (req, res, next) => {
-        const userId = req.params.userId;
-        const user = await accM.getByUserID(userId);
-        if (!user) {
-            next(new HttpError("Invalid user"));
-            return;
-        }
-        res.json(user);
-    },
-    checkUsername: async (req, res, next) => {
-        const username = req.params.username;
-        const acc = await accM.getByUsername(username);
-        console.log
-        if (!acc) {
-            res.json({
-                valid: true
-            })
-        }else {
-            res.json({
-                valid: false
-            })
-        }
-    },
-    signUpHandler: async (req, res, next) => {
-        const un = req.body.username;
-        const acc = await accM.getByUsername(un);
-
-        if (acc) {
-            //console.log(`This username '${acc.Username}' has been existed`);
-            next(new HttpError(`This username '${acc.Username}' has been existed`));
-        } else {
-            const { name, email, password, dob, role } = req.body;
-
-            if (!name || !email || !password || !dob || !role) {
-                next(new HttpError("You have to fill out all the fields"));
-            } else {
-
-                const hashedPw = await bcrypt.hash(password, saltRound);
-                const result = await accM.add(new accM({
-                    Name: name,
-                    Username: un,
-                    Email: email,
-                    Password: hashedPw,
-                    DOB: dob,
-                    Role: role
-                }));
-
-                //console.log(rs);
-                //req.session.auth = true;
-                res.json({
-                    message: "Register new account successfully",
-                    account: {
-                        userId: result.ID,
-                        name: result.Name,
-                        username: result.Username,
-                        email: result.Email
-                    }
-                });
-            }
-
-        }
-    },
-
-    logInHandler: async (req, res, next) => {
-        const { username, password } = req.body;
-
-        const acc = await accM.getByUsername(username);
-        if (!acc) {
-            next(new HttpError(`The username '${username}' does not exist`));
-            return;
-        }
-
-        const check = await bcrypt.compare(password, acc.Password);
-
-        if (!check) {
-            next(new HttpError("Incorrect password"));
-            return;
-        }
-
-        //req.session.auth = true;
-        res.json({ message: "Log in successfully", userId: acc.ID });
-    },
-
-    updateHandler: async (req, res) => {
-        const userID = req.body.ID;
-        const acc = await accM.getByUserID(userID);
-
-        if (!acc) {
-            next(new HttpError("Invalid user id"));
-            return;
-        } else {
-            const newUsername = req.body.newUsername ? req.body.newUsername : null;
-            const newPw = req.body.newPassword ? req.body.newPassword : null;
-            const newName = req.body.newName ? req.body.newName : null;
-            const newEmail = req.body.newEmail ? req.body.newEmail : null;
-            const newDOB = req.body.newDOB ? req.body.newDOB : null;
-            if (!newUsername && !newPw && !newName && !newEmail && !newDOB) {
-                next (new HttpError("You have to provide at least one new information to update"));
-                return;
-            }
-
-            const newValues = {
-                user_id: userID,
-                newUsername,
-                newPassword: newPw ? await bcrypt.hash(newPw, saltRound) : null,
-                newName,
-                newEmail,
-                newDOB
-            }
-            //console.log(newValues);
-            const result = await accM.updateUser(newValues);
-            //console.log("result", result);
-
-            if (result) {
-                res.json({
-                    message: "Update user succesfully",
-                    user: result[0]
-                })
-            }else {
-                next(new HttpError("Fail to update"));
-            }
-        }
-    },
-
-    deleteHandler: async (req, res) => {
-        const {username, password} = req.body;
-        if (!username || !password) {
-            next(new HttpError("Invalid field username or password or both of them"));
-            return;
-        }
-
-        const acc = await accM.getByUsername(username);
-        if (!acc) {
-            next (new HttpError("The user does not exist"));
-            return;
-        }else {
-            let checkPw = false;
-            checkPw = await bcrypt.compare(password, acc.Password);
-            if (checkPw) {
-                await accM.deleteUser(username);
-                res.json({
-                    isSuccess: true,
-                    message: `The user '${username}' has been deleted`
-                })
-            } else {
-                next (new HttpError("Fail to delete the user. The password is incorrect"));
-            }
-        }
+  signUpHandler: async (req, res, next) => {
+    console.log(req.body)
+    const un = req.body.username;
+    console.log(un)
+    const acc = await accM.getByUsername(un);
+    const { name, email, password, dob, role } = req.body;
+    if (acc) {
+      console.log(`This username '${acc.Username}' has been existed`);
+      //console.log(acc);
+      const error = new HttpError(
+        `This username '${acc.Username}' has been existed`,
+        420
+      );
+      return next(error);
     }
-}
+
+    if (!name || !email || !password || !dob || !role) {
+      const error = new HttpError(`You have to fill out all the fields `, 420);
+      return next(error);
+    }
+
+    let newUser;
+    let token;
+    try {
+      const hashedPw = await bcrypt.hash(password, saltRound);
+
+      newUser = await accM.add(
+        new accM({
+          Name: name,
+          Username: un,
+          Email: email,
+          Password: hashedPw,
+          DOB: dob,
+          Role: role,
+        })
+      );
+
+    } catch (err) {
+      const error = new HttpError("Something wrong when signup", 500);
+      return next(error);
+    }
+    // adding token
+    try {
+      token = jwt.sign(
+        {
+          userId : newUser.ID, 
+          username : newUser.Username
+        },
+        process.env.JWT_KEY,
+        {expiresIn : "1h"}
+      );
+    } catch(err) {
+      const error = new HttpError (
+        'Something wrong when add jwt', 500
+      );
+      return next(error);
+    }
+    //console.log(rs);
+    res.status(201).json({
+      message: "Register new account successfully",
+      user: {
+        id: newUser.ID,
+        name: newUser.Name,
+        username: newUser.Username,
+        email: newUser.Email,
+        token : token
+      },
+    });
+  },
+
+  logInHandler: async (req, res, next) => {
+    const { username, password } = req.body;
+
+    const acc = await accM.getByUsername(username);
+    if (!acc) {
+      const error = new HttpError(`The username '${username}' does not exist`, 404);
+      return next(error);
+    }
+
+    const check = await bcrypt.compare(password, acc.Password);
+
+    if (!check) {
+      const error = new HttpError("Incorrect password", 420);
+      return next(error);
+    }
+    try {
+      token = jwt.sign(
+        {
+          userId : acc.ID, 
+          username : acc.Username
+        },
+        process.env.JWT_KEY,
+        {expiresIn : "1h"}
+      );
+    } catch(err) {
+      const error = new HttpError (
+        'Something wrong when add jwt', 500
+      );
+      return next(error);
+    }
+    
+    res.status(201).json({
+      message: "Login success",
+      user: {
+        id: acc.ID,
+        name: acc.Name,
+        username: acc.Username,
+        email: acc.Email,
+        token : token
+      },
+    });
+  },
+
+  updateHandler: async (req, res) => {
+    const userID = req.body.ID;
+    const acc = await accM.getByUserID(userID);
+
+    if (!acc) {
+      res.json({ message: "Invalid user" });
+    } else {
+      const newUsername = req.body.newUsername ? req.body.newUsername : null;
+      const newPw = req.body.newPassword ? req.body.newPassword : null;
+      const newName = req.body.newName ? req.body.newName : null;
+      const newEmail = req.body.newEmail ? req.body.newEmail : null;
+      const newDOB = req.body.newDOB ? req.body.newDOB : null;
+      if (!newUsername && !newPw && !newName && !newEmail && !newDOB) {
+        return res.json({
+          message: "You have to provide at least one new information to update",
+        });
+      }
+
+      const newValues = {
+        user_id: userID,
+        newUsername,
+        newPassword: newPw ? await bcrypt.hash(newPw, saltRound) : null,
+        newName,
+        newEmail,
+        newDOB,
+      };
+      //console.log(newValues);
+      const result = await accM.updateUser(newValues);
+      //console.log("result", result);
+
+      if (result) {
+        res.json({
+          message: "Update user succesfully",
+          user: result[0],
+        });
+      } else {
+        res.json({
+          message: "Fail to update user",
+        });
+      }
+    }
+  },
+
+  deleteHandler: async (req, res) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res.json({
+        isSuccess: false,
+        message: "Invalid field username or password or both of them",
+      });
+    }
+
+    const acc = await accM.getByUsername(username);
+    if (!acc) {
+      return res.json({
+        isSuccess: false,
+        message: "The user does not exist",
+      });
+    } else {
+      let checkPw = false;
+      checkPw = await bcrypt.compare(password, acc.Password);
+      if (checkPw) {
+        await accM.deleteUser(username);
+        res.json({
+          isSuccess: true,
+          message: `The user '${username}' has been deleted`,
+        });
+      } else {
+        res.json({
+          isSuccess: false,
+          message: "Fail to delete the user. The password is incorrect",
+        });
+      }
+    }
+  },
+};
