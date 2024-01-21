19d066b198245d7b3e4d233593d27335785fe66b
21120321 <gamefun206@gmail.com>
2024-01-21 16:19:43 +0700

    Creat Landing

--- Front-end/src/pages/Admin/Landing.js
+++ Front-end/src/pages/Admin/Landing.js
@@ -2,12 +2,14 @@ import React from 'react'
 import AdminHeader from '../../components/Admin/AdminHeader';
 import AdminSidebar from '../../components/Admin/AdminSidebar';
 import ShopChart from '../../components/Admin/components/Chart';
+import Statistic from '../../components/Admin/Statistic';
 function Landing() {
   return (
     <div className="grid-container page-admin">
             <AdminHeader />
             <AdminSidebar />
             <main className="main-container">
+                <Statistic/>
                 <ShopChart/>
             </main>
         </div>
